e9b5548c9a8e4cd38cd991694da0891f4e1f1f1c
DDarkoO <ddejanoski@gmail.com>
2022-09-21 01:35:55 +0200

    private_initial

---
+++ SorsixTasks_1_and_2/SorsixTasks_1_and_2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace MaxMinAlteredNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Your job is to find the smallest and the largest number that can be obtained by switching two digits from a given number.Write a function that takes a number and returns the min and max number in the following format "Min: 551 Max: 155".The number should not start with zero.
+            string userInput = Console.ReadLine();
+            MaxMinNumber(int.Parse(userInput));
+        }
+
+        public static string MaxMinNumber(int number)
+        {
+            if (number.ToString().StartsWith("0")) return "The input number must not start with 0, please try again";
+
+            int max = 0;
+            int min = 0;
+
+            string digits = number.ToString();
+
+            int maxDigit = 0;
+            int minDigit = 10;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (Convert.ToInt32(digits[i].ToString()) > maxDigit)
+                {
+                    maxDigit = Convert.ToInt32(digits[i].ToString());
+                }
+                if (Convert.ToInt32(digits[i].ToString()) < minDigit)
+                {
+                    minDigit = Convert.ToInt32(digits[i].ToString());
+                }
+            }
+
+            //max
+            if (!(int.Parse(digits.ToCharArray()[0].ToString()) == maxDigit))
+            {
+                for (int i = 0; i < digits.Length; i++)
+                {
+                    if (int.Parse(digits[i].ToString()) == maxDigit)
+                    {
+                        max = int.Parse(SwapDigits(digits, i, 0));
+                    }
+                }
+            }
+            else max = number;
+
+            //min
+            if (!(int.Parse(digits.ToCharArray()[0].ToString()) == minDigit))
+            {
+                for (int i = 0; i < digits.Length; i++)
+                {
+                    if (int.Parse(digits[i].ToString()) == minDigit)
+                    {
+                        min = int.Parse(SwapDigits(digits, i, 0));
+                    }
+                }
+            }
+            else min = number;
+
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            return $"Min number: {min}; Max number: {max}";
+        }
+
+        public static string SwapDigits(string number, int index1, int index2)
+        {
+            char[] numberToReturn = number.ToCharArray();
+
+            char helper = number[index1];
+
+            numberToReturn[index1] = number[index2];
+            numberToReturn[index2] = helper;
+
+            return String.Join("", numberToReturn);
+        }
+
+    }
+}
---
+++ SorsixTasks_1_and_2/StringValidation_Task2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectStringsNamespace
+{
+    //Valid String Problem 1 (0 / 0) You are given a string and your job is to write a function that determines if that string is valid by following these rules: The number of times that the letters appear in the string should be the same The string is also correct if by removing only 1 character, the remaining appear the same number of times If a string is valid return "CorrectString", if it is not valid return "Not Correct String"
+
+    public class CorrectStrings
+    {
+        public static string ReadInput()
+        {
+            return Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            string inputLine = ReadInput();
+            IsStringCorrect(inputLine);
+
+        }
+
+        public static string IsStringCorrect(string inputLine)
+        {
+            string trimmed = inputLine.Trim();
+
+            Dictionary<char, int> myList = KeyValuePairs(trimmed);
+
+            for (int i = 0; i < trimmed.Length; i++)
+            {
+                if (myList.ContainsKey(trimmed[i]))
+                {
+                    myList[trimmed[i]]++;
+                }
+            }
+
+            if (myList.Values.Distinct().Count() == 1)
+            {
+                Console.WriteLine("Correct string!");
+                return "Correct string!";
+            }
+            else if (myList.Values.Distinct().Count() == 2)
+            {
+                int validator = myList.Values.Distinct().OrderBy(x => x).First();
+                if (validator == 1 && myList.Select(x => x.Value == 1).ToList().Count == 1)
+                {
+                    Console.WriteLine("Correct string!");
+                    return "Correct string!";
+                }
+                else if (myList.Values.Distinct().OrderByDescending(x => x).First() == (myList.Values.Distinct().OrderByDescending(x => x).Last() + 1))
+                {
+                    Console.WriteLine("Correct string!");
+                    return "Correct string!";
+                }
+                else
+                {
+                    Console.WriteLine("Wrong string!");
+                    return "Wrong string!";
+                }
+            }
+
+            Console.WriteLine("Wrong string!");
+            return "Wrong string!";
+
+        }
+
+        public static Dictionary<char, int> KeyValuePairs(string word)
+        {
+            string nonDuplicates = string.Join("", word.ToCharArray().Distinct());
+
+            Dictionary<char, int> result = new Dictionary<char, int>();
+            foreach (var letter in nonDuplicates)
+            {
+                result[letter] = 0;
+            }
+
+            return result;
+        }
+    }
+}
